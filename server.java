d6efc57ceb96786473d979803101f40329160c8b
Ganesh A <140815684+Ganesh-1710@users.noreply.github.com>
2023-09-14 23:29:47 +0530

    server.java

---
+++ server.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Server {
+    private String id;
+    private String name;
+    private String language;
+    private String framework;
+}
